1b5b3a0542c122aeef3490d41539745d3a7d4a6a
MehirNarayana <mehirkn@gmail.com>
2023-01-21 17:38:25 -0500

    made flask file structure

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="./static")
+
+@app.route('/')
+def index():
+    return render_template('submissionForm.html', templates='templates')
+
+
+
+# main driver function
+if __name__ == '__main__':
+    
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
