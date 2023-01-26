999cbf06e21194454ddd471811a6f2e0c529d5db
MehirNarayana <mehirkn@gmail.com>
2023-01-26 15:06:42 -0500

    Assigned jobs

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 from flask import Flask, render_template
 
+
+
 app = Flask(__name__, static_folder="./static")
 
 @app.route('/')
 def index():
     return render_template('submissionForm.html', templates='templates')
+
+
+
+
 #fsisfd
 #sdkjfklsdjlfsd
 #oeiuwoepq
 #djssdjf
 #ksjflfg
+
+
+
+
+
+@app.route('/form')
+def api():
+    
+
+    #pandas code
+    pass
 
 # main driver function
 if __name__ == '__main__':
