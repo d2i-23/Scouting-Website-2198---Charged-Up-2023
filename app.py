2c00fdb266e69a61b79188c26518483c0480ea69
d2i-23 <clementcycc8@gmail.com>
2023-01-27 20:13:07 -0500

    pandas bugfix

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ mainWorkSheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1NPK8B3CF
 worksheet = mainWorkSheet.get_worksheet(0)
 
 def processRawData(dataFrame):
-    dataFrame = dataFrame.dropna(thresh=10).duplicated(subset = [['Team Number'], ['Match Number']], keep ='first')
-
+    dataFrame = dataFrame.dropna(thresh=10).drop_duplicates(subset = ['Team Number', 'Match Number'], keep = 'first')
     idList = ['Team Number', 'Match Number', 'Lower Cube Scored', 'Middle Cube Scored', 'Upper Cube Scored', 'Lower Cone Scored', 'Upper Cone Scored', 'Middle Cone Scored', 'Lower Auto Score', 'Middle Auto Score', 'Upper Auto Score', 'Lower Total Score', 'Middle Total Score', 'Upper Total Score']
     for i in idList:
-        exec(f'dataFrame[{i}] = dataFrame[{i}].astype("int32")')
+        string = 'dataFrame["' + i + '"] = dataFrame["' + i + '"].astype("int32", errors = "ignore")'
+        exec(string)
     dataFrame['Auto Charge Station'] = 12 if 'engaged' else (8 if 'not engaged' else 0)
     dataFrame['Tele-op Charge Station'] = 10 if 'engaged' else (6 if 'not engaged' else 0)
     dataFrame['W/L'] = dataFrame['W/L'].apply(lambda x: True if x == 'true' else False)
@@ -26,23 +26,20 @@ def processRawData(dataFrame):
 def addRawData(dictionary):
     dataFrame = pd.DataFrame(worksheet.get_all_records())
     testData = dictionary
-    dataframe = processRawData(pd.DataFrame(testData, index = [0]))
-    dataframe = pd.concat([dataFrame,dataframe], ignore_index = True)
+    dataframe = pd.DataFrame(testData, index = [0])
+    dataframe = processRawData(pd.concat([dataFrame,dataframe], ignore_index = True))
     worksheet.clear()
     worksheet.update([dataframe.columns.values.tolist()] + dataframe.values.tolist())
-    processRawData()
 
 def updateAutonomous():
     processData = pd.DataFrame(worksheet.get_all_records())[['Team Number','Lower Auto Score', 'Middle Auto Score', 'Upper Auto Score', 'Auto Charge Station']]
-
-    processData['Auto Total Score'] = processData['Lower Auto Score'] + processData['Middle Auto Score'] + processData['Upper Auto Score'] 
-    processData['AutoScore'] = processData.groupby(by = 'Team Number', as_index = True).apply(lambda x: x.sum(numeric_only = True) / x['Match Number'].count()).sort_values(by = 'Auto Total Score', ascending = False)
-
-    autoWordSheet = mainWorkSheet.get_worksheet(0)
+    processData['Count'] = 1
+    processData['Auto Total Score'] = processData['Lower Auto Score'] + processData['Middle Auto Score'] + processData['Upper Auto Score'] + processData['Auto Charge Station']
+    processData= processData.groupby(by = 'Team Number', as_index = True).apply(lambda x: x.sum(numeric_only = True) / x['Count'].sum()).sort_values(by = 'Auto Total Score', ascending = False)
+    processData.drop('Count', inplace = True, axis = 1)
+    autoWordSheet = mainWorkSheet.get_worksheet(1)
     autoWordSheet.clear()
     autoWordSheet.update([processData.columns.values.tolist()] + processData.values.tolist())
-
-
 
     
 #def updateAutonomous()
@@ -59,6 +56,7 @@ def api():
         req = request.form.to_dict()
         print(req)
         addRawData(req)
+        updateAutonomous()
     return render_template('submissionForm.html', templates='templates')
 
 
